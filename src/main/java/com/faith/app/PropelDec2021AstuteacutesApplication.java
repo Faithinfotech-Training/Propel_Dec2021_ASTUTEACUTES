5ff2457c64c118b53327521a0ec6de4e73e10c1d
dell <cijocyriac@gmail.com>
2022-05-06 15:18:15 +0530

    First Commit

---
+++ src/main/java/com/faith/app/PropelDec2021AstuteacutesApplication.java
@@ -0,0 +1,13 @@
+package com.faith.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropelDec2021AstuteacutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropelDec2021AstuteacutesApplication.class, args);
+	}
+
+}
